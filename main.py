dc13039993d9da80fc915a05f66a9d7545da05cf
yousuf <yosefmahmoud356@gmail.com>
2023-07-06 18:54:39 +0300

    rearrange

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 from simplekivy import SimpleKivy 
+from widgets import (
+    data_inputs
+)
+
 s = SimpleKivy(title="plotter")
 
-FIRST_DICT = {"size_hint":(1, .35), "orientation":"vertical"}
-SECOND_DICT = {"size_hint":(1, None), "height":"70dp", "padding":"4dp"}
 THIRD_DICT = {"size_hint":(1, .65), "orientation":"vertical"}
 
-axis_input = s.TextInput(hint_text="only numsbers")
-yis_input = s.TextInput(hint_text="only numsbers")
-ok_btn = s.Button(text="ok", size_hint=(.4, None), height="50dp", pos_hint={"center_x":.5})
+
 s + [
-    [FIRST_DICT,
-        [SECOND_DICT,
-            axis_input, s.Label(text="axis")
-        ],
-        [SECOND_DICT,
-            yis_input, s.Label(text="wyis")
-        ],
-        ok_btn,    
-    ],
+    data_inputs,
     
     [THIRD_DICT,
 
